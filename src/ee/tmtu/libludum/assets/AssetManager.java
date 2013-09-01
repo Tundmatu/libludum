17bf74b68cf07ac0dfda9d5ac3503b5d076627c0
Felix Kaaman <trundmatu@gmail.com>
2013-09-01 23:21:17 +0200

    Added support for loading assets with different extensions of same type

--- src/ee/tmtu/libludum/assets/AssetManager.java
+++ src/ee/tmtu/libludum/assets/AssetManager.java
@@ -1,10 +1,7 @@
 package ee.tmtu.libludum.assets;
 
 import com.google.gson.Gson;
-import ee.tmtu.libludum.assets.loaders.FontLoader;
-import ee.tmtu.libludum.assets.loaders.LineLoader;
-import ee.tmtu.libludum.assets.loaders.SoundLoader;
-import ee.tmtu.libludum.assets.loaders.TextureLoader;
+import ee.tmtu.libludum.assets.loaders.*;
 import ee.tmtu.libludum.graphics.Font;
 import ee.tmtu.libludum.graphics.Texture;
 import ee.tmtu.libludum.sound.Sound;
@@ -16,7 +13,7 @@ import java.util.Map;
 
 public class AssetManager {
 
-	private static final Map<Class<?>, AssetLoader> assetLoaders = new HashMap<>();
+	private static final Map<Class<?>, AssetReference> assetLoaders = new HashMap<>();
 	private static final Map<String, Object> loadedCache = new HashMap<>();
 
     public static final Gson gson = new Gson();
@@ -25,15 +22,22 @@ public class AssetManager {
 		return load(res, type, assetLoaders);
 	}
 
-	public static <T> T load(String res, Class<T> type, Map<Class<?>, AssetLoader> loaders) {
+	public static <T> T load(String res, Class<T> type, Map<Class<?>, AssetReference> loaders) {
 		File file = new File(res);
         if (loadedCache.containsKey(file.getPath())) {
             return (T) loadedCache.get(file.getPath());
         }
-		AssetLoader loader = loaders.get(type);
+		AssetReference ar = loaders.get(type);
+        AssetLoader al = null;
+        if(!ar.multi) {
+            al = ar.loader;
+        } else {
+            String ext = res.substring(res.lastIndexOf('.', res.length() - 1));
+            al = ar.extensions.get(ext.toLowerCase());
+        }
         T ret = null;
         try {
-            ret = (T) loader.load(file);
+            ret = (T) al.load(file);
             loadedCache.put(file.getPath(), ret);
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,15 +45,15 @@ public class AssetManager {
         return ret;
 	}
 
-	public static <T> void register(Class<?> c, AssetLoader<T> al) {
-		assetLoaders.put(c, al);
+	public static void register(Class<?> c, AssetReference ar) {
+		assetLoaders.put(c, ar);
 	}
 
     static {
-        AssetManager.register(Texture.class, new TextureLoader());
-        AssetManager.register(Font.class, new FontLoader());
-        AssetManager.register(Sound.class, new SoundLoader());
-        AssetManager.register(String[].class, new LineLoader());
+        AssetManager.register(Texture.class, new AssetReference(new TextureLoader()));
+        AssetManager.register(Font.class, new AssetReference(new FontLoader()));
+        AssetManager.register(Sound.class, new AssetReference().extension("wav", new WavLoader()).extension("ogg", new OggLoader()));
+        AssetManager.register(String[].class, new AssetReference(new LineLoader()));
     }
 
 }
